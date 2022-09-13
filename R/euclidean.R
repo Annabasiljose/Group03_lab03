3637d93e6df8224093bade2d9645de6f8b2f3bd8
unknown <niyibizibright@gmail.com>
2022-09-13 14:44:23 +0200

    adding euclidean function

---
+++ R/euclidean.R
@@ -0,0 +1,26 @@
+#euclidean Function
+euclidean <- function(a,b){
+  gcd_result <- 0
+  counter_limit <-0
+  counter <-1
+  if(!is.numeric(a)|| !is.numeric(b)){
+    stop("Input values have to be Numeric")
+  }
+  #Find the last number for our loop
+
+  if(abs(a)>abs(b)){
+    counter_limit <-abs(b)
+  }else{
+    counter_limit <-abs(a)
+  }
+  #Find the Greatest Common Divisor
+  while(counter<=counter_limit){
+
+    if((abs(a) %% counter == 0) && (abs(b) %% counter == 0)) {
+      gcd_result = counter
+    }
+    counter <-counter+1
+  }
+  #Return Greatest Common Divisor
+  return(gcd_result)
+}
