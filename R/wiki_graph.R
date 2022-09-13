132b19ab654d78c6e7d150b8c6727e9b0c73f525
Annabasiljose <annabasiljose1@gmail.com>
2022-09-13 17:08:01 +0200

    Unit test integration

--- R/wiki_graph.R
+++ R/wiki_graph.R
@@ -6,8 +6,6 @@
 #'
 #' @keywords datasets
 #'
-#' @references Wikipedia
-#' (\href{https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm})
-#'
+#' @references \url{https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm}
 #'
 "wiki_graph"
