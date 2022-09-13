9b43604bcaa2c0b3364505e1a169e20f82c22e29
Annabasiljose <annabasiljose1@gmail.com>
2022-09-13 16:41:56 +0200

    Included dataset and its documentation

---
+++ R/wiki_graph.R
@@ -0,0 +1,13 @@
+#' Wiki_graph dataa
+#'
+#' Dataframe
+#'
+#' @docType data
+#'
+#' @keywords datasets
+#'
+#' @references Wikipedia
+#' (\href{https://en.wikipedia.org/wiki/Dijkstra%27s_algorithm})
+#'
+#'
+"wiki_graph"
