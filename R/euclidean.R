a981ad3ec434f20f4c15dec28304f3d52cf4b9fe
unknown <niyibizibright@gmail.com>
2022-09-16 13:17:51 +0200

    Euclidean Algorithm

--- R/euclidean.R
+++ R/euclidean.R
@@ -1,34 +1,20 @@
 #' Euclidean function to calculate the Greatest Common Divisor(GCD)
 #' @param a the first numeric scalar/integer argument
 #' @param b the second numeric scalar/integer argument
-#' @return calculated Greatest Common Divisor(GCD) from arguments(a and b)
+#' @return Calculated Greatest Common Divisor(GCD) from arguments(a and b)
 #' @references
 #' \url{https://en.wikipedia.org/wiki/Euclidean}
 #' @export
 
 #euclidean Function
-euclidean <- function(a,b){
-  gcd_result <- 0
-  counter_limit <-0
-  counter <-1
-  if(!is.numeric(a)|| !is.numeric(b)){
-    stop("Input values have to be Numeric")
-  }
-  #Find the last number for our loop
 
-  if(abs(a)>abs(b)){
-    counter_limit <-abs(b)
-  }else{
-    counter_limit <-abs(a)
+euclidean <- function (a,b){
+  while(b>0){
+    t<-b
+    b<- a%%b
+    a <-t
   }
-  #Find the Greatest Common Divisor
-  while(counter<=counter_limit){
 
-    if((abs(a) %% counter == 0) && (abs(b) %% counter == 0)) {
-      gcd_result = counter
-    }
-    counter <-counter+1
-  }
-  #Return Greatest Common Divisor
-  return(gcd_result)
+  return (a)
 }
+
